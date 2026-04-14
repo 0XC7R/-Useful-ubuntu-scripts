58ee0760f76bcddd42cedced014102da3d54dd36
0XC7R <0xC7R@proton.me>
2026-04-14 13:13:58 +0100

    Update FixUsbDrive.sh

--- FixUsbDrive.sh
+++ FixUsbDrive.sh
@@ -1,2 +1,6 @@
 sudo modprobe --ignore-install usb_storage
 sudo modprobe uas
+
+# fixes this isue: 
+#uas: Unknown symbol usb_stor_sense_invalidCDB (err -2)
+#uas: Unknown symbol usb_stor_adjust_quirks (err -2)
